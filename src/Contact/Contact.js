75157eeab3946281b9aa71edf3694554642bf718
AMIT AKOLKAR <akolkar.amit.445@gmail.com>
2020-05-04 11:36:42 +0530

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,7 +4,13 @@ class Contact extends Component {
   render() {
     return (
       <div className="">
-        <div>Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact Contact </div>
+        <div>
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tortor nunc, rhoncus vitae commodo non, mattis eu ante. Cras gravida eleifend ante, ut ornare nulla egestas id. Sed urna arcu, malesuada vitae tristique quis, vestibulum a neque. Nullam volutpat diam non neque blandit, at commodo ante vestibulum. Donec in est facilisis, malesuada sem nec, interdum ligula. Donec consequat sapien in iaculis rutrum. Proin mi risus, ultrices sed nisi a, blandit interdum nibh.
+
+Vivamus sit amet maximus sapien. Morbi accumsan diam non urna dapibus, ac faucibus orci rutrum. Phasellus neque diam, lacinia at lobortis a, gravida nec magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pulvinar elit sed ex condimentum sagittis. Sed dictum sit amet enim tempus rhoncus. Donec vel neque magna. </div>
+<h1>By @Amit Akolkar</h1>
       </div>
     );
   }
